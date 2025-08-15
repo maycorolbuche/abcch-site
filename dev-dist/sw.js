0d16f7ef0bd0c5a0047f566e6081fbb167ddfb25
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-08-15 12:44:57 -0300

    Ajuste - otimização

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r8chudt8h48"
+    "revision": "0.pnui8phi1j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
