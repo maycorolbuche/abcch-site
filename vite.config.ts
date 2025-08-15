9d75c47938190006450cee56f805d6c83132c9e5
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-08-15 12:27:28 -0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve,dirname  } from 'path';
 import { fileURLToPath } from 'url';
 import Components from 'unplugin-vue-components/vite'
 import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
+import { VitePWA } from "vite-plugin-pwa";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -13,6 +14,48 @@ export default defineConfig({
     vue(),
     Components({
       resolvers: [BootstrapVueNextResolver()],
+    }),
+    VitePWA({
+      registerType: "autoUpdate", // Registra o Service Worker para atualizar automaticamente
+      devOptions: {
+        enabled: true, // Ativa o PWA também durante o desenvolvimento
+      },
+      workbox: {
+        globPatterns: ["**/*.{html,js,css,svg,png}"], // Arquivos que o Service Worker deve cachear
+      },
+      manifest: {
+        name: "ABCCH - Associação Brasileira dos Criadores do Cavalo de Hipismo",
+        short_name: "ABCCH",
+        description: "Associação Brasileira dos Criadores do Cavalo Brasileiro de Hipismo",
+        start_url: process.env.VITE_BASE_URL ?? "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: (process.env.VITE_BASE_URL ?? "/") + "favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: (process.env.VITE_BASE_URL ?? "/") + "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src:
+              (process.env.VITE_BASE_URL ?? "/") + "favicon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src:
+              (process.env.VITE_BASE_URL ?? "/") + "favicon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   server: {
