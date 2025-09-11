19a5c02862e93552c52134a765877ff271294c81
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-09-11 17:08:00 -0300

    Ajuste contorno

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uru4pidgnl"
+    "revision": "0.rs4keq6vi3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/home/BlocksLinks.vue
+++ src/components/home/BlocksLinks.vue
@@ -92,8 +92,7 @@ export default {
       font-size: 17px;
       font-weight: bold;
       text-align: center;
-      text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000,
-        2px 2px 0 #000, 2px 0 0 #000, 0 2px 0 #000, -2px 0 0 #000, 0 -2px 0 #000;
+      text-shadow: 3px 3px 2px #000000;
     }
 
     &:hover {
