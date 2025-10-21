2c4187bfa01171f611fb7fcac7c4f72f371bfb31
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-10-21 15:20:25 -0300

    Ajuste noticias

--- src/components/home/LatestNews.vue
+++ src/components/home/LatestNews.vue
@@ -12,18 +12,18 @@
     <template v-if="data">
       <BCard v-for="item in data" :key="item.id" no-body class="border-0 my-3">
         <BRow class="g-0">
-          <BCol cols="4" class="d-flex align-items-center">
+          <BCol cols="3" class="d-flex align-items-center">
             <BCardImg
               :src="item.imagem_url"
               alt="Image"
               :rounded="false"
               lazy
-              style="object-fit: cover; max-height: 100px; background: #eee"
+              style="object-fit: cover; height: 100px; background: #eee"
             />
           </BCol>
-          <BCol cols="8">
+          <BCol cols="9">
             <BCardBody class="py-0">
-              <BCardTitle class="fs-5 text-truncate">
+              <BCardTitle class="fs-5">
                 <span class="fw-700 text-uppercase">
                   {{ item.dia_mes_publicacao }}:
                 </span>
@@ -32,7 +32,7 @@
                 </span>
               </BCardTitle>
               <BCardText class="fs-14px">
-                <span v-html="item.resumo" />
+                <span v-if="item.resumo != '...'" v-html="item.resumo" />
                 <router-link
                   :to="{ name: 'read_news', params: { id: item.id } }"
                   class="float-end fw-600 fs-13px"
