fa018806254cff3ce5bc4230da4f5b4bb4ab5907
maycorolbuche <mayco_rolbuche@hotmail.com>
2026-02-19 10:43:47 -0300

    Busca por inicial em Garanhões

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9kovfmaorb"
+    "revision": "0.v3u83paf448"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/Stallions.vue
+++ src/views/Stallions.vue
@@ -1,41 +1,61 @@
 <template>
   <Page title="Garanhões" subtitle="Aprovados e licenciados pela ABCCH">
     <div class="px-5">
-      <DataTable
-        api-url="/garanhoes"
-        searchLabel="Informe o nome, o registro ou microchip"
-        searchLabelFilter="Nome"
-        :fields="[
-          {
-            key: 'NmAnimal',
-            label: 'Animal',
-            route: {
-              to: 'animal',
-              params: { id: 'CdToken' },
-            },
-          },
-          {
-            key: 'NrRegistration',
-            label: 'Registro',
-          },
-          {
-            key: 'DtFoaledBr',
-            label: 'Dt. Nasc.',
-          },
-          {
-            key: 'NmAnimalSire',
-            label: 'Pai',
-          },
-          {
-            key: 'NmAnimalDam',
-            label: 'Mãe',
-          },
-          {
-            key: 'NmUserOwner',
-            label: 'Proprietário',
-          },
-        ]"
-      />
+      <div class="d-flex">
+        <div class="d-flex flex-column mt-5 pt-4 me-4">
+          <BButton
+            v-for="letter in letters"
+            :key="letter"
+            @click="setInicial(letter)"
+            :variant="letter == initial ? 'primary' : 'outline-primary'"
+            pill
+            class="p-0 m-1 fw-600"
+            style="width: 30px; height: 30px"
+          >
+            {{ letter }}
+          </BButton>
+        </div>
+
+        <div style="flex: 1">
+          <DataTable
+            api-url="/garanhoes"
+            searchLabel="Informe o nome, o registro ou microchip"
+            searchLabelFilter="Nome"
+            :params="{ initial }"
+            :filters="[{ label: 'Inicial', value: initial }]"
+            :fields="[
+              {
+                key: 'NmAnimal',
+                label: 'Animal',
+                route: {
+                  to: 'animal',
+                  params: { id: 'CdToken' },
+                },
+              },
+              {
+                key: 'NrRegistration',
+                label: 'Registro',
+              },
+              {
+                key: 'DtFoaledBr',
+                label: 'Dt. Nasc.',
+              },
+              {
+                key: 'NmAnimalSire',
+                label: 'Pai',
+              },
+              {
+                key: 'NmAnimalDam',
+                label: 'Mãe',
+              },
+              {
+                key: 'NmUserOwner',
+                label: 'Proprietário',
+              },
+            ]"
+          />
+        </div>
+      </div>
     </div>
   </Page>
 </template>
@@ -44,6 +64,7 @@
 import Page from "@/components/Page.vue";
 
 import DataTable from "@/components/DataTable.vue";
+import Storage from "@/helpers/Storage.js";
 
 export default {
   components: {
@@ -51,7 +72,25 @@ export default {
     DataTable,
   },
   data: () => ({
-    uf: null,
+    initial: null,
   }),
+  computed: {
+    letters() {
+      return Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
+    },
+  },
+  watch: {
+    initial() {
+      Storage.set("dt-initial-/garanhoes", this.initial);
+    },
+  },
+  methods: {
+    async setInicial(letter) {
+      this.initial = this.initial !== letter ? letter : null;
+    },
+  },
+  mounted() {
+    this.initial = Storage.get("dt-initial-/garanhoes", "");
+  },
 };
 </script>
